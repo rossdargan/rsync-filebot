81b3fe46c86070f9b7d0a751e92a66aafdb32ba6
Ross Dargan <git@the-dargans.co.uk>
2017-06-07 23:42:54 +0100

    Updated to change the way cron is worked

--- entry.sh
+++ entry.sh
@@ -18,11 +18,14 @@ ssh-add /root/.ssh/id_rsa
 # Add the cronfile to fxleblanc's cron
 crontab cron/sync
 
+#copy cron/sync /etc/cron.d/sync 
+
 # Launch the script once if ONE_SHOT is defined
 echo "Starting cron"
-cron
-echo "Following these logs:"
-ls /var/logs/*
-echo "A restart might be needed to follow new logs"
-tail -q /var/logs/* -f
+cron 
+#echo "Following these logs:"
+#ls /var/logs/*
+#echo "A restart might be needed to follow new logs"
+touch /var/log/cron.log
+tail /var/log/cron.log -f
 
